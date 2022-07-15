8492c5abf20a55dfab0efaad80d24773d3400940
KWON HYUKJIN <hyukjin_89@naver.com>
2022-07-16 00:12:52 +0900

    Update: hardcoded clicker

--- JavaScript/JavaScript-Design-Patterns/Clicker/index.js
+++ JavaScript/JavaScript-Design-Patterns/Clicker/index.js
@@ -1,18 +1,45 @@
-// Requirements 
+// Requirements
 
-// UI 
-// Show 2 image and number of clicks 
-// the name should be above the image. 
+// UI
+// Show 2 image and number of clicks
+// the name should be above the image.
 
 // Function
 // The number of clicks should increment when image is clicked.
 
-const box = document.querySelector(".box");
-const counter = document.querySelector(".counter");
-let count = 1
+const app = document.querySelector("#app");
+const page = document.createElement("div");
 
-box.addEventListener('click', () => {
-    counter.innerHTML = count++;
+let countFirst = 0;
+let countSecond = 0;
+
+this.render = () => {
+  page.innerHTML = `
+    <div style="float:left" >
+        <h2>box1<h2>
+        <div class="box first"></div> 
+        <span class="firstValue">${countFirst}</span>
+    </div>
+    <div style="display:inline-block">
+        <h2>box2<h2>
+        <div class="box second"></div>
+        <span class="secondValue">${countSecond}</span>
+    </div>
+    `;
+
+  app.appendChild(page);
+};
+
+render();
+
+const first = page.querySelector(".first");
+first.addEventListener("click", () => {
+  countFirst++;
+  document.querySelector(".firstValue").innerHTML = countFirst;
 });
 
-console.log(app);
+const second = page.querySelector(".second");
+second.addEventListener("click", () => {
+  countSecond++;
+  document.querySelector(".secondValue").innerHTML = countSecond;  
+});
